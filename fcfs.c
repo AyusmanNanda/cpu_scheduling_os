4eb42e2094620fa929979ae73a17eed030e810c0
IC7400A <abhiseknanda799@gmail.com>
2025-09-28 22:07:47 +0530

    Complete implemantation of FCFS(i think :))

--- fcfs.c
+++ fcfs.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -13,17 +14,38 @@ struct proc{
 		int ta_time;
 };
 
+int compareByAT(const void *a, const void *b){
+	const struct proc *p1 = (const struct proc *)a;
+    const struct proc *p2 = (const struct proc *)b;
+    if (p1->a_time < p2->a_time) return -1;
+    else if (p1->a_time > p2->a_time) return 1;
+    else return 0;
+
+
+}
 
 int fcfs(int proc_count){
-		srand(time(NULL));	
+		srand(time(NULL)); //seed 	
+		
+		// create a bunch of process
 		struct proc process[proc_count];
 		for(int i = 0; i < proc_count ; i++){
 				process[i].proc_id = i + 1;
 				process[i].a_time = rand() % 101;
 		}
+		printf("Process before sorting: \n");
 		for(int i = 0; i < proc_count; i++){
 				printf("process no %d : process id [%d], arival time [%d]\n", i, process[i].proc_id, process[i].a_time);
 		}
+		// Using quick sort
+		qsort(process, proc_count, sizeof(struct proc), compareByAT);
+
+		// Print after sorting
+    	printf("\nProcess after sorting by arrival time:\n");
+    	for(int i = 0; i < proc_count; i++){
+        	printf("process no %d : process id [%d], arrival time [%d]\n", i, process[i].proc_id, process[i].a_time);
+    	}
+
 		return 0;
 
 }

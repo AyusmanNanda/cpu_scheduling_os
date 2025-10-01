1f3508c8bdb7c4c598dbc090fa960c2448c589fe
IC7400A <abhiseknanda799@gmail.com>
2025-10-01 09:33:52 +0530

    Inprovement: Added initial fcfs. simplified the function compareByAT and renamed it to compareArrivalTime for simplisity . Inproved naming . Added extended comments in the main.c

---
+++ sjfs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sjfs.h"
+
+
+int sjfs(int proc_count, struct proc process[]){
+    
+}
---
+++ sjfs.h
@@ -0,0 +1,8 @@
+#ifdef SJFS_H
+#define SJFS_H
+
+#include "procgen.h" // Defined to be able to use the procgen logic.
+
+int sjfs(int proc_count, struct proc process[]);
+
+#endif // SJFS_H
